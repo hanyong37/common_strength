d2bfc635c10ee490096d718029c6c2d5529c17f5
chenxi <chenxi@chenxideMacBook-Air.local>
2016-10-25 21:09:15 +0800

    add customer name and user name to operation

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,4 +1,13 @@
 class Operation < ApplicationRecord
   belongs_to :customer
   belongs_to :user
+
+
+  def customer_name
+    self.customer.name
+  end
+
+  def user_name
+    self.user.name
+  end
 end
--- app/serializers/operation_serializer.rb
+++ app/serializers/operation_serializer.rb
@@ -1,3 +1,3 @@
 class OperationSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :customer_id,  :description, :operation_memo,:created_at
+  attributes :id, :user_id, :user_name, :customer_id, :customer_name,   :description, :operation_memo,:created_at
 end
