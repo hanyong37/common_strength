3f978b4fc5c77b62f3fb49ec594381f29b5e720e
chenxi <chenxi@chenxideMacBook-Air.local>
2016-11-19 23:21:45 +0800

    预约和取消时间限制分开；

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -3,7 +3,8 @@ class Setting < ApplicationRecord
     booking_limit_days: 2,  #允许用户提前？天预定
     course_view_days: 3,    #允许用户查看？天的课程
     cancel_limit_minutes: 4,#允许用户提前？分钟可以取消，以及自动确认排队；
-    queue_limit_number: 5  #排队人数限制
+    queue_limit_number: 5 , #排队人数限制
+    booking_limit_minutes: 1#允许用户提前？分钟可以取消，以及自动确认排队；
   }
 
   def self.booking_limit_days
@@ -36,6 +37,14 @@ class Setting < ApplicationRecord
 
   def self.queue_limit_number=(value)
     Setting.find_by_key(:queue_limit_number).update(value: value.to_s)
+  end
+
+  def self.booking_limit_minutes
+    Setting.find_by_key(:booking_limit_minutes).value.to_i
+  end
+
+  def self.booking_limit_minutes=(value)
+    Setting.find_by_key(:booking_limit_minutes).booking_limit_minutes(value: value.to_s)
   end
 
 end
