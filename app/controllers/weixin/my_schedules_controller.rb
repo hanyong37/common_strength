d509a3181031357c2faedc188ff54a0bf4050ca9
chenxi <chenxi@chenxideMacBook-Air.local>
2016-11-24 00:21:02 +0800

    fix sql date issue

--- app/controllers/weixin/my_schedules_controller.rb
+++ app/controllers/weixin/my_schedules_controller.rb
@@ -1,16 +1,16 @@
 class Weixin::MySchedulesController < Weixin::ApplicationController
   def index
-    set_schedules(Date.today)
+    get_schedules(Date.today)
     render json: @schedules
   end
 
   def show
-    set_schedules(params[:id])
+    get_schedules(params[:id])
     render json: @schedules
   end
 
   private
-  def set_schedules (date)
+  def get_schedules (date)
     @schedules = Schedule.viewable.by_store(@current_customer.store_id).by_date(date).time_asc
   end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  START_DATE_IN_CST = " date(schedules.start_time AT TIME ZONE 'CST') ".freeze
+  START_DATE_IN_CST = " date(schedules.start_time AT TIME ZONE 'utc') ".freeze
 end
